69e273aa508caaa71d2707c3ac342a8821a91925
Gauri Phatate <107755121+Gauri9977@users.noreply.github.com>
2023-09-30 19:05:21 +0530

    Create music_list.js

---
+++ music_list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+  {
+    name: "Fur Elise",
+    artist: "Beethoven",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Intersellar",
+    artist: "Hans Zimmer",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Lux Aterna",
+    artist: "Clint Mansell",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Lose Yourself",
+    artist: "Eminem",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Moonlight Sonata",
+    artist: "Beethoven",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Radioactive",
+    artist: "Imagine dragons",
+    img: "music-6",
+    src: "music-6"
+  }
+];
